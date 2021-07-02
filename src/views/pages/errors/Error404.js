ae07ce3f954acb22049bf998079f59591eb4430e
Bruna A Braguin <andradevbruna@gmail.com>
2021-07-01 22:41:44 -0300

    Add 404  page

--- src/views/pages/errors/Error404.js
+++ src/views/pages/errors/Error404.js
@@ -1,18 +1,28 @@
-let Error404 = {
-    is_private: false,
-    render: async () => {
-        let view = `${error}`
-        return view
-    },
+import errorBg from '../../../assets/images/backgrounds/bg-3.jpeg';
+import darthError from '../../../assets/images/elements/404_darth-vader.png';
 
-    after_render: () => {
-        console.log('Try another one... :)')
-    }
-}
+let Error404 = {
+  is_private: false,
+  render: async () => {
+    let view = `${error}`;
+    return view;
+  },
+
+  after_render: () => {
+    console.log('Try another one... :)');
+  },
+};
 let error = `
-<div>
-    erro 404!
+<div class="error-default" style="background-image: url('${errorBg}')">
+    <div class="container">
+        <h1>erro 404!</h1>
+        <a href="http://localhost:8080/">
+            <img src="${darthError}" alt="Vader error">
+        <a>
+        <h2>Caro padawan, este contéudo não existe.</h2>
+        <h3>Clique no Vader e tente novamente.</h3>
+    </div>
 </div>
-`
+`;
 
 export default Error404;
